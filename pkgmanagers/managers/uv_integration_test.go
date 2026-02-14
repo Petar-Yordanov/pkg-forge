49d166c5d8b4dda603ddfc0889f0a8a49384d9a7
Petar Yordanov <peter.yordanov.dev@gmail.com>
2026-02-14 17:26:03 +0200

    Added: Placeholder test files

---
+++ pkgmanagers/managers/uv_integration_test.go
@@ -0,0 +1,3 @@
+//go:build linux || windows || darwin
+
+package managers_test
--- pkgmanagers/registry.go
+++ pkgmanagers/registry.go
@@ -1,6 +1,12 @@
 package pkgmanagers
 
-import "github.com/Petar-Yordanov/pkg-forge/pkgmanagers/managers"
+import (
+	"strings"
+
+	"github.com/Petar-Yordanov/pkg-forge/pkgmanagers/managers"
+)
+
+type Manager = managers.Manager
 
 func DefaultManagers() []managers.Manager {
 	return []managers.Manager{
@@ -18,4 +24,25 @@ func DefaultManagers() []managers.Manager {
 		managers.Winget{},
 		managers.Pip{},
 	}
+}
+
+var managerByID = func() map[string]Manager {
+	m := make(map[string]Manager, 32)
+	for _, inst := range DefaultManagers() {
+		id := strings.TrimSpace(strings.ToLower(inst.ID()))
+		if id == "" {
+			continue
+		}
+		m[id] = inst
+	}
+	return m
+}()
+
+func InstanceFromString(id string) (Manager, bool) {
+	id = strings.TrimSpace(strings.ToLower(id))
+	if id == "" {
+		return nil, false
+	}
+	m, ok := managerByID[id]
+	return m, ok
 }
